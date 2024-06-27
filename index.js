f8acc7ce4f6a815b7e383e87de8d9bac9fc7e372
zoe <joheun2390@gmail.com>
2024-06-27 21:38:35 +0900

    fixed untracked files

--- index.js
+++
@@ -1,7 +0,0 @@
-let input = require("fs").readFileSync("example.txt").toString().split(" ");
-var a = parseInt(input[0]);
-var b = parseInt(input[1]);
-
-if (a > b) return console.log(">");
-else if (a < b) return console.log("<");
-else return console.log("==");
